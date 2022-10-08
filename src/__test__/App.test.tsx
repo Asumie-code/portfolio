6c8f0067f5e3c9b9ef16d1b31a817250d281ac68
Asumie-code <darkeangel.1998.ia@gmail.com>
2022-10-08 11:55:45 +0100

    fix: change test to allow deployment

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import Nav from '../component/Nav';
+import {HashRouter} from 'react-router-dom'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const Nav = screen.getAllByText(/About/i);
+  render(<HashRouter><Nav /></HashRouter>);
+  const component = screen.getAllByText(/About/i);
   
-    for(let value of Nav) {
+    for(let value of component) {
       expect(value).toBeInTheDocument()
     }
 });
