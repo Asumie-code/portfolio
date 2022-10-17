7c8eb992688fc2876d3e8b00f9957df296d1b855
Asumie-code <darkeangel.1998.ia@gmail.com>
2022-10-17 18:40:54 +0100

    feat: add chart component
     - add charts to skill views to display skill level
     - style charts

---
+++ src/component/Chart.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import LinearProgress from '@mui/material/LinearProgress'
+interface chartProp {
+  value: number,
+  intervals?: number,
+  color?: string, 
+  backgroundColor?: string, 
+  speed?: number
+
+}
+
+const Chart = ({speed = 10,  value = 0, intervals = 100, backgroundColor = 'white', color = 'blue' }: chartProp): JSX.Element => {
+  const [progress, setProgress] = useState<number>(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        const increment = speed;
+        console.log(oldProgress)
+        return oldProgress + increment;
+      });
+    }, intervals);
+    if (progress >= value) {
+      clearInterval(timer)
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [intervals, value, progress]);
+
+  return (
+    <LinearProgress
+      sx={{
+        backgroundColor: backgroundColor,
+        '& > span': {
+          backgroundColor: color
+        }
+      }}
+      variant="determinate"
+      value={progress}
+    />
+
+
+  );
+}
+
+
+
+export default Chart 
--- src/views/Skills.tsx
+++ src/views/Skills.tsx
@@ -1,26 +1,36 @@
-import { Paper, Slider, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import Box from "@mui/material/Box"
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import Chart from '../component/Chart'
 
 
 const skills = [
     {
         name: 'Front-end',
-        value: 75
+        value: 85,
+        color:  '#B3D3FF',
+         
+
     },
     {
         name: 'Back-end',
-        value: 75
+        value: 60,
+        color: '#ED4D47',
+         
     },
     {
         name: 'ReactJS',
-        value: 50
+        value: 78,
+        color: 'primary',
+         
     },
     {
         name: 'NodeJS',
-        value: 50
+        value: 65,
+        color: '#2BAD43',
+         
     },
 ]
 
@@ -30,87 +40,94 @@ const Skills = (): JSX.Element => {
 
     return (
         <>
-                <Box
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: matches ? 'column' : 'row',
+                    '& > :not(style)': {
+                        px: 5,
+                        py: 6,
+                        width: matches ? 1 : 0.5
+                    },
+                }}
+            >
+                <Paper
                     sx={{
-                        display: 'flex',
-                        flexDirection: matches ? 'column' : 'row' ,
-                        '& > :not(style)': {
-                            px: 5,
-                            py: 6,
-                            width: matches ? 1 : 0.5
-                        },
+                        pl: 5
                     }}
                 >
-                    <Paper
+                    <Typography
+                        color='primary'
+                        variant='h2'
+                        component='h2'
                         sx={{
-                            pl: 5
+                            mb: '1.5rem',
+                            fontSize: matches ? '2.8rem' : '3.75rem'
                         }}
                     >
-                        <Typography
-                            color='primary'
-                            variant='h2'
-                            component='h2'
-                            sx={{
-                                mb: '1.5rem',
-                                fontSize: matches ? '2.8rem' :  '3.75rem'
-                            }}
-                        >
-                            Lorem,<br /> ipsum dolor <br /> sit amet.
-                        </Typography>
-                        <Typography
-                            component='p'
-                            sx={{
-                                mb: '1.5rem',
-                                fontSize: matches?  '0.8rem' : '1rem'
-                            }}
-                        >
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis provident sint neque cumque, quas perspiciatis modi eveniet magnam, natus aliquid ipsum distinctio ex exercitationem asperiores.
-                        </Typography>
-                        <Typography
-                            component='p'
-                            sx={{
-                                mb: '1.5rem',
-                                fontSize: matches?  '0.8rem' : '1rem'
-                            }}
-                        >
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis provident sint neque cumque, quas perspiciatis modi eveniet magnam, natus aliquid ipsum distinctio ex exercitationem asperiores. distinctio ex exercitationem asperiores
-                        </Typography>
-                        <Typography
-                            component='p'
-                            sx={{
-                                mb: '1.5rem',
-                                fontSize: matches?  '0.8rem' : '1rem'
-                            }}
-                        >
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis provident sint neque cumque,
-                        </Typography>
-
-                    </Paper>
-                    <Paper
+                        Lorem,<br /> ipsum dolor <br /> sit amet.
+                    </Typography>
+                    <Typography
+                        component='p'
                         sx={{
-
+                            mb: '1.5rem',
+                            fontSize: matches ? '0.8rem' : '1rem'
                         }}
                     >
-                        <List>
-                            {skills.map((skill) => (
-                                <ListItem key={skill.name} sx={{display: 'inline-block'}}>
-                                    <Typography
-                                        variant='h6'
-                                        component='h6'
-                                        sx={{
-                                            fontSize: matches ? '1rem' : '1.25rem'
-                                        }}
-                                    >
-                                        {skill.name}
-                                    </Typography>
-                                    <Slider disabled defaultValue={skill.value} aria-label="Disabled slider" />
-                                </ListItem>
-                            ))}
-                        </List>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis provident sint neque cumque, quas perspiciatis modi eveniet magnam, natus aliquid ipsum distinctio ex exercitationem asperiores.
+                    </Typography>
+                    <Typography
+                        component='p'
+                        sx={{
+                            mb: '1.5rem',
+                            fontSize: matches ? '0.8rem' : '1rem'
+                        }}
+                    >
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis provident sint neque cumque, quas perspiciatis modi eveniet magnam, natus aliquid ipsum distinctio ex exercitationem asperiores. distinctio ex exercitationem asperiores
+                    </Typography>
+                    <Typography
+                        component='p'
+                        sx={{
+                            mb: '1.5rem',
+                            fontSize: matches ? '0.8rem' : '1rem'
+                        }}
+                    >
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis provident sint neque cumque,
+                    </Typography>
+
+                </Paper>
+                <Paper
+                    sx={{
+
+                    }}
+                >
+                    <List>
+                        {skills.map((skill, index) => (
+                            <ListItem key={skill.name} sx={{ display: 'inline-block', mb: 5 }}>
+                                <Typography
+                                    variant='h6'
+                                    component='h6'
+                                    color={skill.color}
+                                    sx={{
+                                        fontSize: matches ? '1rem' : '1.25rem',
+                                        mb: 1
+                                    }}
+                                >
+                                    {skill.name}
+                                </Typography>
+                                <Chart
+                                    value={skill.value}
+                                    backgroundColor='#373737'
+                                    color={skill.color}
+                                    speed={15 - (4 * index)}
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
 
 
-                    </Paper>
-                </Box>
+                </Paper>
+            </Box>
 
         </>
     )
